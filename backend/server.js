66daf4197bd6730738c213d7ae53306bc120b3d2
JANUPALLISAHITHI <sahithijanupalli@gmail.com>
2025-05-03 11:46:27 +0530

    user

--- backend/server.js
+++ backend/server.js
@@ -258,27 +258,27 @@
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
-// import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import userRoutes from "./routes/userRoutes.js";
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes.js";
 
-// dotenv.config();
-// const app = express();
+dotenv.config();
+const app = express();
 
-// app.use(express.json());
-// app.use(cors());
+app.use(express.json());
+app.use(cors());
 
-// // MongoDB Connection
-// mongoose.connect(process.env.MONGO_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// .then(() => console.log("Connected to MongoDB"))
-// .catch(err => console.error(err));
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error(err));
 
-// app.use("/users", userRoutes);
+app.use("/users", userRoutes);
 
-// const PORT = process.env.PORT || 5001;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
